7b79fa6ac0a658658490adb5eaa66392a640f71e
anjalidiggi <162709416+anjalidiggi@users.noreply.github.com>
2024-03-28 20:09:00 +0530

    pyspark practice

--- dataframe/stringfunctions.py
+++ dataframe/stringfunctions.py
@@ -1,4 +1,4 @@
-#Uppercase the Names:
+
 
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import upper
@@ -11,31 +11,29 @@ columns = ["name", "age"]
 df = spark.createDataFrame(data, schema=columns)
 
 df.withColumn("upper_name", upper(df["name"])).show()
-#Concatenate Name and Age
 
 
 from pyspark.sql.functions import concat, lit
 
 df.withColumn("name_age", concat(df["name"], lit("_"), df["age"])).show()
-#Substring of Names
+
 
 
 from pyspark.sql.functions import substring
 
 df.withColumn("name_substring", substring(df["name"], 1, 3)).show()
-#Length of Names
+
 
 
 from pyspark.sql.functions import length
 
 df.withColumn("name_length", length(df["name"])).show()
-#Split Names
 
 
 from pyspark.sql.functions import split
 
 df.withColumn("name_split", split(df["name"], "")).show()
-#Replace Characters in Names
+
 
 
 from pyspark.sql.functions import regexp_replace
