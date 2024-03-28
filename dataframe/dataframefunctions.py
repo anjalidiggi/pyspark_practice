c284e9ee7d99e7a17a030bc8c7579141b5352362
anjalidiggi <162709416+anjalidiggi@users.noreply.github.com>
2024-03-28 20:10:46 +0530

    pyspark practice

--- dataframe/dataframefunctions.py
+++ dataframe/dataframefunctions.py
@@ -1,4 +1,4 @@
-#creatig a dataframe
+
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("DataFrameExample").getOrCreate()
@@ -9,31 +9,25 @@ columns = ["name", "age"]
 df = spark.createDataFrame(data, schema=columns)
 
 df.show()
-#Selecting Columns:\
 
 
 df.select("name").show()
-#Filtering Data
+
 
 df.filter(df["age"] > 21).show()
-#Adding a New Column
 
 
 df.withColumn("age_plus_10", df["age"] + 10).show()
-#Grouping and Aggregating
 
 
 df.groupBy("name").agg({"age": "avg"}).show()
-#Sorting Data
 
 
 df.orderBy("age").show()
-#Joining DataFrames
 
 
 df2 = spark.createDataFrame([("Anjali", "Sales"), ("Akanksha", "Marketing")], ["name", "department"])
 df.join(df2, "name").show()
-#Pivoting Data
 
 
 data = [("Anjali", "Sales", 1000), ("Akanksha", "Marketing", 1500), ("Anjali", "Marketing", 2000)]
@@ -41,7 +35,7 @@ columns = ["name", "department", "salary"]
 df = spark.createDataFrame(data, schema=columns)
 
 df.groupBy("name").pivot("department").sum("salary").show()
-#User-defined Functions (UDFs)
+
 
 
 from pyspark.sql.functions import udf
