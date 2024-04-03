6616749841c3fcc4acdb0c5f3bb3790f90c64e50
anjalidiggi <162709416+anjalidiggi@users.noreply.github.com>
2024-04-03 19:37:01 +0530

    pyspark practice

---
+++ dataframe/union.py
@@ -0,0 +1,23 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import Row
+
+spark = SparkSession.builder.appName("Union").getOrCreate()
+
+data_anjali = [Row(id=1, name='Anjali'), Row(id=2, name='Anjali')]
+data_abhishek = [Row(id=3, name='Abhishek'), Row(id=1, name='Abhishek')]
+data_meera = [Row(id=4, name='Meera'), Row(id=5, name='Meera')]
+data_akanksha = [Row(id=6, name='Akanksha'), Row(id=7, name='Akanksha')]
+data_amit = [Row(id=8, name='Amit'), Row(id=9, name='Amit')]
+
+anjali = spark.createDataFrame(data_anjali)
+abhishek = spark.createDataFrame(data_abhishek)
+meera = spark.createDataFrame(data_meera)
+akanksha = spark.createDataFrame(data_akanksha)
+amit = spark.createDataFrame(data_amit)
+
+combined_df = anjali.union(abhishek)
+
+print("Union:")
+combined_df.show()
+
+spark.stop()
